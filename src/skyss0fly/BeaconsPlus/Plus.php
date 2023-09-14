d184a63412cda4cde05e37a9fe9daea4e9e530c9
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2023-09-14 17:12:57 +1000

    Update Plus.php

--- src/skyss0fly/BeaconsPlus/Plus.php
+++ src/skyss0fly/BeaconsPlus/Plus.php
@@ -7,4 +7,20 @@ use pocketmine\plugin\PluginBase;
 
 
 class Main extends PluginBase{
+
+  public function getBeacons(){
+$this->file_get_contents("Beacons.yml");
+    $this->loadBeacons();
+    
+  }
+
+  public function construct() {
+    $commands = 'bp';
+    $subcommands = 'ui', 'manager';
+    $permissions = 'BeaconsPlus.ui', 'BeaconsPlus.manager';
+
+    $this->getPluginManager()->registerCommands($commands);
+    $this->getPluginManager()->registerSubcommands($subcommands);
+    $this->getPluginManager()->registerPermissions($permissions);
+    
 }
